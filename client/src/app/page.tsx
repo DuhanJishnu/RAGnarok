1cfc052af61c88457a447c2b4b72e56421ca3223
Shashank Umar Vaishy <umarshashank@gmail.com>
2025-09-27 11:35:20 +0530

    finished merge

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,12 +1,21 @@
-'use client'
-import ChatWindow from '@/components/ChatWindow'
-
+"use client";
+import ChatWindow from "@/components/ChatWindow";
+import Header from "@/components/Header";
+import Sidebar from "@/components/Sidebar";
 
 export default function Home() {
   return (
-      <div className="h-full w-full">
-      <ChatWindow />
-      </div>
-    
-  )
+    <div className="bg-gray-50 text-gray-900 dark:bg-[#0f1724] dark:text-gray-100">
+            <div className="min-h-screen flex">
+              <Sidebar />
+              <div className="flex-1 flex flex-col">
+                <Header />
+                {/* <main className="flex-1"><ChatWindow/></main> */}
+                <div className="h-full w-full">
+                  <ChatWindow />
+                </div>
+              </div>
+            </div>
+          </div>
+  );
 }
