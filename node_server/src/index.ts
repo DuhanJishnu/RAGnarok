acaa5d6db88e353ccc2d1edb6432d7b91bd07b7c
Shashank Umar Vaishy <umarshashank@gmail.com>
2025-10-01 21:24:37 +0530

    no cors

--- node_server/src/index.ts
+++ node_server/src/index.ts
@@ -13,9 +13,7 @@ const port = PORT
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors({
-  credential: true
-}));
+app.use(cors());
 app.use(cookieParser());
 
 app.get("/", (req: Request, res: Response) => {
